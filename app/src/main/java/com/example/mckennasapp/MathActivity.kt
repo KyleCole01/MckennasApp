add94d85000a62082d958ddd64a111f77051fc93
KyleCole01 <kyle@misfitselectronics.com>
2019-08-28 15:53:27 -0400

    successfully map the math solver and math formulas button to their activities

--- app/src/main/java/com/example/mckennasapp/MathActivity.kt
+++ app/src/main/java/com/example/mckennasapp/MathActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mckennasapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_math.*
 
 class MathActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_math)
+      button_mathactivity_formulas.setOnClickListener {
+          startActivity(Intent(this, MathFormulasActivity::class.java))
+      }
+        button_mathactivity_solver.setOnClickListener {
+            startActivity(Intent(this,MathSolverActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/mckennasapp/MathFormulasActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mckennasapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_math.*
+
+class MathFormulasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_math_formulas)
+
+    }
+}
